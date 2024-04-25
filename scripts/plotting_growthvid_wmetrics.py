6e949ff5cbee687b1bd6a029b052e4204b2c7050
Clement Sebastiao <clement.sebastiao@gmail.com>
2024-04-25 10:57:55 +0200

    Add AUC computing and plotting

--- scripts/plotting_growthvid_wmetrics.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Script to plot growth video attached to metric evolution on the side.
-"""
-
-
-if __name__ == "__main__":
-    # TODO Finish it
-    folderoots = "./data/processed/ignored_files/utg"
